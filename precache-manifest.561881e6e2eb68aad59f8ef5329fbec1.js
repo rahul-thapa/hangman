e707157e329e667b9ca18dca6b462e9c1ac367b0
Rahul Thapa <rahulthapa.work@gmail.com>
2020-03-25 19:27:07 +0530

    Updates

---
+++ precache-manifest.561881e6e2eb68aad59f8ef5329fbec1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d57d58fd03aa89957803f0320407ccd",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "510b841518701fa9dbed",
+    "url": "/hangman/static/css/main.85f917cf.chunk.css"
+  },
+  {
+    "revision": "f49ae41d7860e0837c58",
+    "url": "/hangman/static/js/2.b964f4dd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hangman/static/js/2.b964f4dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510b841518701fa9dbed",
+    "url": "/hangman/static/js/main.97277931.chunk.js"
+  },
+  {
+    "revision": "6bcab2d1b8a15260d101",
+    "url": "/hangman/static/js/runtime-main.4f9ac029.js"
+  },
+  {
+    "revision": "90488b1c9052659e6e7bed06232effc0",
+    "url": "/hangman/static/media/3.90488b1c.png"
+  },
+  {
+    "revision": "214592d24e4a248e6223023eb08ab2cb",
+    "url": "/hangman/static/media/4.214592d2.png"
+  },
+  {
+    "revision": "11b07d1bdd7f410ac1abb5e42171358d",
+    "url": "/hangman/static/media/5.11b07d1b.png"
+  },
+  {
+    "revision": "07612ecefefdc8d65fe8458ef4f244a4",
+    "url": "/hangman/static/media/6.07612ece.png"
+  }
+]);
